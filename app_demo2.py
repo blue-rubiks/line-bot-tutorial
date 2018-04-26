cc822b324f4d3f9f78d5703f7f6a7d276573184b
blue-rubiks <blue555236sa56@gmail.com>
2018-04-26 10:57:22 +0800

    update

--- app_demo2.py
+++ app_demo2.py
@@ -155,6 +155,10 @@ def handle_message(event):
             carousel_template_message)
         return 0
 
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
 
 @handler.add(MessageEvent, message=StickerMessage)
 def handle_sticker_message(event):
@@ -164,7 +168,7 @@ def handle_sticker_message(event):
     sticker_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 21, 100, 101, 102, 103, 104, 105, 106,
                    107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125,
                    126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 401, 402]
-    index_id = random.randint(0, len(sticker_ids)-1)
+    index_id = random.randint(0, len(sticker_ids) - 1)
     sticker_id = str(sticker_ids[index_id])
     print(index_id)
     sticker_message = StickerSendMessage(
